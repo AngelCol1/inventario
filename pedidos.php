1a2837930d1ade2102d3262d310f60e1f9176f52
ANGELF <colangel743@gmail.com>
2022-07-10 11:16:13 -0600

    Diseño de Formularios

---
+++ pedidos.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>ventas</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+
+
+<!-- DataTales Example -->
+<div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Pedidos</h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre de Producto</th>
+                                <th>Marca</th>
+                                <th>Descripcion</th>
+                                <th>Tienda</th>
+                                <th>Proveedor</th>
+                                <th>Cantidad</th>
+                                <th>Costo Total</th>
+                                <th>ACCIONES</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                            <th>ID</th>
+                                <th>Nombre de Producto</th>
+                                <th>Marca</th>
+                                <th>Descripcion</th>
+                                <th>Tienda</th>
+                                <th>Proveedor</th>
+                                <th>Cantidad</th>
+                                <th>Costo Total</th>
+                                <th>ACCIONES</th>
+                            </tr>
+                        </tfoot>
